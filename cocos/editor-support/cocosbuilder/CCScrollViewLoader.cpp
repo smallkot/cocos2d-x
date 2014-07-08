98d53e3056d30d1b29a351a9923fc3ab34489542
Sergey <non_2001@mail.ru>
2014-07-08 14:51:18 +0400

    ScrollViewLoader fix

--- cocos/editor-support/cocosbuilder/CCScrollViewLoader.cpp
+++ cocos/editor-support/cocosbuilder/CCScrollViewLoader.cpp
@@ -8,8 +8,38 @@ using namespace cocos2d::extension;
 #define PROPERTY_CLIPSTOBOUNDS "clipsToBounds"
 #define PROPERTY_BOUNCES "bounces"
 #define PROPERTY_SCALE "scale"
+#define PROPERTY_CONTENTNODE "contentNode"
+#define PROPERTY_VERTICALSCROLLENABLED "verticalScrollEnabled"
+#define PROPERTY_HORIZONTALSCROLLENABLED "horizontalScrollEnabled"
 
 namespace cocosbuilder {
+    
+void ScrollViewLoader::onStarPropertiesParsing(cocos2d::Node * pNode, CCBReader * ccbReader)
+{
+    _verticalScrollEnabled = false;
+    _horizontalScrollEnabled = false;
+}
+    
+void ScrollViewLoader::onEndPropertiesParsing(cocos2d::Node * pNode, CCBReader * ccbReader)
+{
+    if(ccbReader->getVersion()>5)
+    {
+        if(_verticalScrollEnabled)
+        {
+            if(_horizontalScrollEnabled)
+                ((ScrollView *)pNode)->setDirection(ScrollView::Direction::BOTH);
+            else
+                ((ScrollView *)pNode)->setDirection(ScrollView::Direction::VERTICAL);
+        }
+        else
+        {
+            if(_horizontalScrollEnabled)
+                ((ScrollView *)pNode)->setDirection(ScrollView::Direction::HORIZONTAL);
+            else
+                ((ScrollView *)pNode)->setDirection(ScrollView::Direction::NONE);
+        }
+    }
+}
 
 void ScrollViewLoader::onHandlePropTypeSize(Node * pNode, Node * pParent, const char * pPropertyName, Size pSize, CCBReader * ccbReader) {
 	if(strcmp(pPropertyName, PROPERTY_CONTENTSIZE) == 0) {
@@ -24,13 +54,17 @@ void ScrollViewLoader::onHandlePropTypeCheck(Node * pNode, Node * pParent, const
         ((ScrollView *)pNode)->setClippingToBounds(pCheck);
     } else if(strcmp(pPropertyName, PROPERTY_BOUNCES) == 0) {
         ((ScrollView *)pNode)->setBounceable(pCheck);
+    } else if(strcmp(pPropertyName, PROPERTY_VERTICALSCROLLENABLED) == 0) {
+        _verticalScrollEnabled = pCheck;
+    } else if(strcmp(pPropertyName, PROPERTY_HORIZONTALSCROLLENABLED) == 0) {
+        _horizontalScrollEnabled = pCheck;
     } else {
         NodeLoader::onHandlePropTypeCheck(pNode, pParent, pPropertyName, pCheck, ccbReader);
     }
 }
 
 void ScrollViewLoader::onHandlePropTypeCCBFile(Node * pNode, Node * pParent, const char * pPropertyName, Node * pCCBFileNode, CCBReader * ccbReader) {
-    if(strcmp(pPropertyName, PROPERTY_CONTAINER) == 0) {
+    if((strcmp(pPropertyName, PROPERTY_CONTAINER) == 0) || (strcmp(pPropertyName, PROPERTY_CONTENTNODE) == 0)) {
         ((ScrollView *)pNode)->setContainer(pCCBFileNode);
 		((ScrollView *)pNode)->updateInset();
     } else {
--- cocos/editor-support/cocosbuilder/CCScrollViewLoader.h
+++ cocos/editor-support/cocosbuilder/CCScrollViewLoader.h
@@ -24,11 +24,16 @@ public:
 
 protected:
     CCB_VIRTUAL_NEW_AUTORELEASE_CREATECCNODE_METHOD(cocos2d::extension::ScrollView);
+    virtual void onStarPropertiesParsing(cocos2d::Node * pNode, CCBReader * ccbReader) override;
+    virtual void onEndPropertiesParsing(cocos2d::Node * pNode, CCBReader * ccbReader) override;
     virtual void onHandlePropTypeSize(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, cocos2d::Size pSize, CCBReader * ccbReader) override;
     virtual void onHandlePropTypeCCBFile(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, cocos2d::Node * pCCBFileNode, CCBReader * ccbReader) override;
     virtual void onHandlePropTypeCheck(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, bool pCheck, CCBReader * ccbReader) override;
     virtual void onHandlePropTypeFloat(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, float pFloat, CCBReader * ccbReader) override;
     virtual void onHandlePropTypeIntegerLabeled(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, int pIntegerLabeled, CCBReader * ccbReader) override;
+private:
+    bool _verticalScrollEnabled;
+    bool _horizontalScrollEnabled;
 };
 
 }
